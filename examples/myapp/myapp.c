b26dcc52ce866587a9572e4736ec6cf17b8a5fdc
Tahmidur Rahman Tabin <trtabin@gmail.com>
2025-10-26 14:07:14 +0600

    Create pthead in the main task

--- examples/myapp/myapp.c
+++ examples/myapp/myapp.c
@@ -1,42 +1,28 @@
-/****************************************************************************
- * apps/examples/hello/hello_main.c
- *
- * SPDX-License-Identifier: Apache-2.0
- *
- * Licensed to the Apache Software Foundation (ASF) under one or more
- * contributor license agreements.  See the NOTICE file distributed with
- * this work for additional information regarding copyright ownership.  The
- * ASF licenses this file to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance with the
- * License.  You may obtain a copy of the License at
- *
- *   http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
- * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  See the
- * License for the specific language governing permissions and limitations
- * under the License.
- *
- ****************************************************************************/
-
-/****************************************************************************
- * Included Files
- ****************************************************************************/
-
 #include <nuttx/config.h>
+#include <pthread.h>
 #include <stdio.h>
+#include <unistd.h>
 
-/****************************************************************************
- * Public Functions
- ****************************************************************************/
-
-/****************************************************************************
- * hello_main
- ****************************************************************************/
-
-int main(int argc, FAR char *argv[])
+void *thread_func(void *arg)
 {
-  printf("Hello from myapp demo!!\n");
+  while (1)
+    {
+      printf("Thread %ld running\n", (long)arg);
+      sleep(2);
+    }
+  return NULL;
+}
+
+int main(int argc, char *argv[])
+{
+  pthread_t t1, t2;
+  pthread_create(&t1, NULL, thread_func, (void *)1);
+  pthread_create(&t2, NULL, thread_func, (void *)2);
+
+  while (1)
+    {
+      printf("Main task running\n");
+      sleep(3);
+    }
   return 0;
 }
